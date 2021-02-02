f19b13ebbb4dfb0108bf548d3a027fc4200c43c8
Marc <47401977+MarcM29@users.noreply.github.com>
2021-02-02 13:44:12 -0500

    Added Search functionality and authorization

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Problem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyWebApp.Models
+{
+    public class Problem
+    {
+        public int Id { get; set; }
+        public string ProblemQuestion { get; set; }
+        public string ProblemAnswer { get; set; }
+        public Problem()
+        {
+
+        }
+
+    }
+}
