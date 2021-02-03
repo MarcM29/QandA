b9dafb7ab97b5e963e095d14f44f5233b62cf2c8
Marc <47401977+MarcM29@users.noreply.github.com>
2021-02-03 17:56:44 -0500

    Added error detection for creating problem questions

--- Models/Problem.cs
+++ Models/Problem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace StudyWebApp.Models
     public class Problem
     {
         public int Id { get; set; }
+        [Display(Name = "Practice problem:")]
+        [Required(ErrorMessage = "Practice problem field may not be left empty")]
         public string ProblemQuestion { get; set; }
+        [Display(Name = "Problem Solution:")]
+        [Required(ErrorMessage = "Problem solution field may not be left empty")]
         public string ProblemAnswer { get; set; }
+        [Display(Name = "Username of problem submitter:")]
         public string UsersName { get; set; }
 
         public Problem()
